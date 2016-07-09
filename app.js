b068c9f08d734862eb1cc07e3575a437e9cf75f8
Maxis Kao <maxiskao@yahoo-inc.com>
2016-07-09 20:36:50 +0800

    debug

--- app.js
+++ app.js
@@ -76,15 +76,15 @@ app.get('/webhook', function(req, res) {
  */
 app.post('/webhook', function (req, res) {
 
-console.log('=====================================================================');
+// console.log('=====================================================================');
 
-console.log('>>> app.post');
+// console.log('>>> app.post');
 
   var data = req.body;
 
   // Make sure this is a page subscription
 
-console.log('>>> data.object:', data.object);
+// console.log('>>> data.object:', data.object);
 
   if (data.object == 'page') {
     // Iterate over each entry
@@ -191,58 +191,67 @@ function receivedAuthentication(event) {
  * then we'll simply confirm that we've received the attachment.
  * 
  */
+// function receivedMessage(event) {
+
+// console.log('>>> receivedMessage');
+
+//   var senderID = event.sender.id;
+//   var recipientID = event.recipient.id;
+//   var timeOfMessage = event.timestamp;
+//   var message = event.message;
+
+//   console.log("Received message for user %d and page %d at %d with message:", 
+//     senderID, recipientID, timeOfMessage);
+//   // console.log(JSON.stringify(message));
+
+// console.log('>>>>>> before switch');
+
+//   var messageId = message.mid;
+
+//   // You may get a text or attachment but not both
+//   var messageText = message.text;
+//   var messageAttachments = message.attachments;
+
+//   if (messageText) {
+
+//     // If we receive a text message, check to see if it matches any special
+//     // keywords and send back the corresponding example. Otherwise, just echo
+//     // the text we received.
+
+//     switch (messageText) {
+//       case 'image':
+//         sendImageMessage(senderID);
+//         break;
+
+//       case 'button':
+//         sendButtonMessage(senderID);
+//         break;
+
+//       case 'generic':
+//         sendGenericMessage(senderID);
+//         break;
+
+//       case 'receipt':
+//         sendReceiptMessage(senderID);
+//         break;
+
+//       default:
+//         console.log('>>>>>>>>> before gulis');
+//         gulis(senderID, messageText);
+//         // sendTextMessage(senderID, messageText);
+//     }
+
+//   } else if (messageAttachments) {
+//     sendTextMessage(senderID, "Message with attachment received");
+//   }
+// }
+
 function receivedMessage(event) {
-
-console.log('>>> receivedMessage');
-
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
   var message = event.message;
-
-  console.log("Received message for user %d and page %d at %d with message:", 
-    senderID, recipientID, timeOfMessage);
-  // console.log(JSON.stringify(message));
-
-console.log('>>>>>> before switch');
-
-  var messageId = message.mid;
-
-  // You may get a text or attachment but not both
+  var senderID = event.sender.id;
   var messageText = message.text;
-  var messageAttachments = message.attachments;
-
   if (messageText) {
-
-    // If we receive a text message, check to see if it matches any special
-    // keywords and send back the corresponding example. Otherwise, just echo
-    // the text we received.
-
-    switch (messageText) {
-      case 'image':
-        sendImageMessage(senderID);
-        break;
-
-      case 'button':
-        sendButtonMessage(senderID);
-        break;
-
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
-      case 'receipt':
-        sendReceiptMessage(senderID);
-        break;
-
-      default:
-        console.log('>>>>>>>>> before gulis');
-        gulis(senderID, messageText);
-        // sendTextMessage(senderID, messageText);
-    }
-
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
+    gulis(senderID, messageText);
   }
 }
 
@@ -303,7 +312,7 @@ function gulis(recipientId, messageText) {
     var messageData = '';
     var res;
 
-console.log('>>> messageText:', messageText);
+// console.log('>>> messageText:', messageText);
 
     request.post(uri, {
         form: {
@@ -314,7 +323,7 @@ console.log('>>> messageText:', messageText);
 
         var beautyBody = JSON.parse(body);
 
-console.log('>>> beautyBody:', beautyBody);
+// console.log('>>> beautyBody.text:', beautyBody);
 
         if (beautyBody.text) {
           res = beautyBody.text.split('\n');
