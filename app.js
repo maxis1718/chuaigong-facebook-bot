97d4fc581637ef9c26cd862ffc64ec8ad8cb881d
Maxis Kao <maxiskao@yahoo-inc.com>
2016-07-13 20:29:10 +0800

    generic

--- app.js
+++ app.js
@@ -279,11 +279,55 @@ function gulis(recipientId, message, messageType) {
             userid: recipientId,
             message: message || '',
             platform: 'facebook',
-            type: messageType
+            type: messageType,
+            user: true
         }
     }, function (error, response, body) {
 
-        console.log('[gulis] body:', body);
+        var messageData = {
+            recipient :{
+                id: recipientId
+            },
+            message: {
+              attachment: {
+                type: "template",
+                payload: {
+                  template_type: "generic",
+                  elements: [{
+                    title: "rift",
+                    subtitle: "Next-generation virtual reality",
+                    item_url: "https://www.oculus.com/en-us/rift/",               
+                    image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+                    buttons: [{
+                      type: "web_url",
+                      url: "https://www.oculus.com/en-us/rift/",
+                      title: "Open Web URL"
+                    }, {
+                      type: "postback",
+                      title: "Call Postback",
+                      payload: "Payload for first bubble",
+                    }],
+                  }, {
+                    title: "touch",
+                    subtitle: "Your Hands, Now in VR",
+                    item_url: "https://www.oculus.com/en-us/touch/",               
+                    image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+                    buttons: [{
+                      type: "web_url",
+                      url: "https://www.oculus.com/en-us/touch/",
+                      title: "Open Web URL"
+                    }, {
+                      type: "postback",
+                      title: "Call Postback",
+                      payload: "Payload for second bubble",
+                    }]
+                  }]
+                }
+              }
+            }
+        };
+
+        callSendAPI(messageData);
 
         // extract response
         /*
@@ -292,7 +336,7 @@ function gulis(recipientId, message, messageType) {
           res = beautyBody.text.split('\n');
         }*/
 
-        sendTextMessage(recipientId, '收到');
+        //sendTextMessage(recipientId, '收到');
     });
 }
 
